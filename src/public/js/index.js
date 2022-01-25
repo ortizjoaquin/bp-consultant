1e5e6c53d6380fb686f85aa4e2d94bc4e6a9bc51
joaquinortiz <joaquin.ortizdlc@gmail.com>
2022-01-25 16:04:45 +0100

    added mobile first design, working on collapse sidepanel

---
+++ src/public/js/index.js
@@ -0,0 +1,7 @@
+function openNav() {
+  document.getElementById("mobileSidepanel").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mobileSidepanel").style.width = "0";
+}
